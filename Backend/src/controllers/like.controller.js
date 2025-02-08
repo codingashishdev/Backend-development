f01468629e6aecc462c4e460543bf2669455ef19
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2025-02-08 16:03:14 +0530

    Implement like functionality for videos, comments, and tweets with error handling and retrieval of liked videos

--- Backend/src/controllers/like.controller.js
+++ Backend/src/controllers/like.controller.js
@@ -6,23 +6,70 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params
-    //TODO: toggle like on video
+
+    if (!videoId) {
+        throw new ApiError(400, "videoId is required")
+    }
+
+    if (!mongoose.Types.isValidObjectId(videoId)) {
+        throw new ApiError(400, "A valid objectId has not been given")
+    }
+
+    const likedVideo = await Like.findByIdAndUpdate({
+        video: videoId
+    })
+
+    return res.status(200).json(new ApiResponse(200, "Video liked successfully"))
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const { commentId } = req.params
-    //TODO: toggle like on comment
+    if (!commentId) {
+        throw new ApiError(400, "commentId is required")
+    }
 
+    if (!mongoose.Types.isValidObjectId(commentId)) {
+        throw new ApiError(400, "A valid objectId has not been given")
+    }
+
+    const likedComment = await Like.findByIdAndUpdate({
+        comment: commentId
+    })
+
+    if (!likedComment) {
+        throw new ApiError(400, "Error while like comment")
+    }
+
+    return res.status(200).json(new ApiResponse(200, "Comment liked successfully"))
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const { tweetId } = req.params
-    //TODO: toggle like on tweet
-}
-)
+
+    if (!tweetId) {
+        throw new ApiError(400, "tweetId is required")
+    }
+
+    if (!mongoose.Types.isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Not a valid object id")
+    }
+
+    const likedTweet = await Like.findByIdAndUpdate({
+        tweet: tweetId
+    })
+
+    return res.status(200).json(new ApiResponse(200, "Tweet liked successfully"))
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    const allLiked = await Like.find().populate('video')
+    const likedVideos = allLiked.map((like) => like.video)
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, likedVideos, "Liked videos fetched successfully")
+        )
 })
 
 export {
