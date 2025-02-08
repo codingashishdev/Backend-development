534e0ad917faebf19f88c8e62fdc385f998ce4af
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2025-02-08 13:38:12 +0530

    Implement tweet creation, retrieval, updating, and deletion functionalities with error handling

--- Backend/src/controllers/tweet.controller.js
+++ Backend/src/controllers/tweet.controller.js
@@ -1,4 +1,4 @@
-import mongoose, { isValidObjectId } from "mongoose"
+import mongoose, { isValidObjectId, mongo } from "mongoose"
 import { Tweet } from "../models/tweet.model.js"
 import { User } from "../models/user.model.js"
 import { ApiError } from "../utils/ApiError.js"
@@ -6,19 +6,84 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+    const { content } = req.body
+
+    if (!content?.trim()) {
+        throw new ApiError(404, "Content not found")
+    }
+
+    const tweet = await Tweet.create({
+        content: content,
+        owner: req.User?._id
+    })
+
+    if (!tweet) {
+        throw new ApiError(400, "Error while creating tweet")
+    }
+
+    return res.status(200).json(new ApiResponse(200, tweet, "Tweet created successfully"))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+    const { userId } = req.params;
+    
+    if (!userId) {
+        throw new ApiError(400, "userId is required")
+    }
+
+    if (!mongoose.Types.isValidObjectId(userId)) {
+        throw new ApiError(400, "A valid objectId has not been given")
+    }
+
+    const userTweets = await Tweet.find({
+        owner: userId
+    })
+
+    if (!userTweets) {
+        throw new ApiError(400, "Error while fetching user tweets")
+    }
+
+    return res.status(200).json(new ApiResponse(200, {}, "user tweets fetched successfully"))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    const { updatedContent, tweetId } = req.body
+
+    if (!updatedContent || !tweetId) {
+        throw new ApiError(400, "updatedContent and tweetId are required")
+    }
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            content: updatedContent
+        },
+        {
+            new: true
+        }
+    )
+
+    if (!updateTweet) {
+        throw new ApiError(400, "Error while updating tweet")
+    }
+
+    return res.status(200).json(new ApiResponse(200, updatedTweet, "tweet updated successfully"))
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const { tweetId } = req.body;
+
+    if (!tweetId) {
+        throw new ApiError(400, "tweetId is required")
+    }
+
+    const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
+
+    if (!deletedTweet) {
+        throw new ApiError(400, "Something went wrong while deleting tweet")
+    }
+
+    return res.status(200).json(new ApiResponse(200, deletedTweet, "tweet deleted successfully"))
 })
 
 export {
