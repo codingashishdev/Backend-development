6784d96234a17b84c9c39751a6243bb9972870fb
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2025-02-09 20:32:48 +0530

    Implement subscription management functionalities including toggling subscriptions, fetching channel subscribers, and retrieving subscribed channels with error handling

--- Backend/src/controllers/subscription.controller.js
+++ Backend/src/controllers/subscription.controller.js
@@ -5,20 +5,79 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
-
 const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params
-    // TODO: toggle subscription
+    if (!channelId) {
+        throw new ApiError(400, "channelId is required")
+    }
+
+    if (!mongoose.Types.isValidObjectId(channelId)) {
+        throw new ApiError(400, "not a valid objectId")
+    }
+
+    const sub = await Subscription.create({
+        subscriber: req.User?._id,
+        channel: channelId
+    })
+
+    if (!sub) {
+        throw new ApiError(400, "Error while subscribing")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, sub, "channel subscribed successfully"))
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const { channelId } = req.params
+
+    if (!channelId) {
+        throw new ApiError(400, "channelId is required")
+    }
+
+    if (!mongoose.Types.isValidObjectId(channelId)) {
+        throw new ApiError(400, "not a valid objectId")
+    }
+
+    const channelSubscribers = await Subscription.find(
+        { channel: channelId }
+    )
+
+    if (!channelSubscribers) {
+        throw new ApiError(400, "Errow while fetching channel subscribers")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, channelSubscribers, "channel subscribers fetched successfully"))
+
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if (!subscriberId) {
+        throw new ApiError(400, "subscriberId is required")
+    }
+
+    if (!mongoose.Types.isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "not a valid objectId")
+    }
+
+    const channelsSubscribedByUser = await Subscription.find(
+        {
+            subscriber: subscriberId
+        }
+    )
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, channelsSubscribedByUser, "User subscribed channels fetched successfully")
+        )
 })
 
 export {
