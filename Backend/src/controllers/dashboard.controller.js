204ce6c81bbd9c345bd5c63b3d99117a8705b440
codingashishdev <atankmember@gmail.com>
2025-02-12 16:17:02 +0530

    Implement channel statistics retrieval and subscription management functionalities with error handling

--- Backend/src/controllers/dashboard.controller.js
+++ Backend/src/controllers/dashboard.controller.js
@@ -1,20 +1,71 @@
-import mongoose from "mongoose"
-import { Video } from "../models/video.model.js"
-import { Subscription } from "../models/subscription.model.js"
-import { Like } from "../models/like.model.js"
-import { ApiError } from "../utils/ApiError.js"
-import { ApiResponse } from "../utils/ApiResponse.js"
-import { asyncHandler } from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc
-})
+    const { userId } = req.body?._id;
+
+    if (!userId) {
+        throw new ApiError(400, "userId is required");
+    }
+
+    if (!mongoose.Types.isValidObjectId(userId)) {
+        throw new ApiError(400, "Not a valid objectId");
+    }
+
+    const videoViews = await Video.aggregate(
+        { $match: { owner: userId } },
+        {
+            $project: {
+                views: 1,
+            },
+        }
+    );
+
+    const totalSubscribers = await Subscription.countDocuments({ channel: userId });
+
+    if (!totalSubscribers) {
+        throw new ApiError("Error while getting channel subscribers")
+    }
+
+    const totalLikes = await Like.countDocuments({ video })
+
+    if (!totalLikes) {
+        throw new ApiError(400, "Error while fetching total likes");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, [videoViews, totalSubscribers, totalLikes], "Channel stats fetched successfully")
+        )
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
+    const { userId } = req.body._id;
 
-export {
-    getChannelStats,
-    getChannelVideos
-}
+    if (!userId) {
+        throw new ApiError(400, "userId is required")
+    }
+
+    if (!mongoose.Types.isValidObjectId(userId)) {
+        throw new ApiError(400, "Not a valid objectId")
+    }
+
+    const channelVideos = await Video.find(
+        { owner: userId }
+    )
+
+    if (!channelVideos) {
+        throw new ApiError(400, "Something went wrong while fetching channel videos")
+    }
+
+    return res.status(200).json(new ApiResponse(200, channelVideos, "Channel videos fetched successfully"))
+});
+
+export { getChannelStats, getChannelVideos };
--- Backend/src/controllers/subscription.controller.js
+++ Backend/src/controllers/subscription.controller.js
@@ -1,87 +1,97 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import { User } from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
-import { ApiError } from "../utils/ApiError.js"
-import { ApiResponse } from "../utils/ApiResponse.js"
-import { asyncHandler } from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const { channelId } = req.params
+    const { channelId } = req.params;
     if (!channelId) {
-        throw new ApiError(400, "channelId is required")
+        throw new ApiError(400, "channelId is required");
     }
 
     if (!mongoose.Types.isValidObjectId(channelId)) {
-        throw new ApiError(400, "not a valid objectId")
+        throw new ApiError(400, "not a valid objectId");
+    }
+
+    if (
+        Subscription.exists({
+            subscriber: req.User?._id,
+            channel: channelId,
+        })
+    ) {
+        throw new ApiError(400, "Subscription already exists");
     }
 
     const sub = await Subscription.create({
         subscriber: req.User?._id,
-        channel: channelId
-    })
+        channel: channelId,
+    });
 
     if (!sub) {
-        throw new ApiError(400, "Error while subscribing")
+        throw new ApiError(400, "Error while subscribing");
     }
 
     return res
         .status(200)
-        .json(new ApiResponse(200, sub, "channel subscribed successfully"))
-})
+        .json(new ApiResponse(200, sub, "channel subscribed successfully"));
+});
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const { channelId } = req.params
+    const { channelId } = req.params;
 
     if (!channelId) {
-        throw new ApiError(400, "channelId is required")
+        throw new ApiError(400, "channelId is required");
     }
 
     if (!mongoose.Types.isValidObjectId(channelId)) {
-        throw new ApiError(400, "not a valid objectId")
+        throw new ApiError(400, "not a valid objectId");
     }
 
-    const channelSubscribers = await Subscription.find(
-        { channel: channelId }
-    )
+    const channelSubscribers = await Subscription.find({ channel: channelId });
 
     if (!channelSubscribers) {
-        throw new ApiError(400, "Errow while fetching channel subscribers")
+        throw new ApiError(400, "Error while fetching channel subscribers");
     }
-
-    return res
-        .status(200)
-        .json(new ApiResponse(200, channelSubscribers, "channel subscribers fetched successfully"))
-
-})
-
-// controller to return channel list to which user has subscribed
-const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-
-    if (!subscriberId) {
-        throw new ApiError(400, "subscriberId is required")
-    }
-
-    if (!mongoose.Types.isValidObjectId(subscriberId)) {
-        throw new ApiError(400, "not a valid objectId")
-    }
-
-    const channelsSubscribedByUser = await Subscription.find(
-        {
-            subscriber: subscriberId
-        }
-    )
 
     return res
         .status(200)
         .json(
-            new ApiResponse(200, channelsSubscribedByUser, "User subscribed channels fetched successfully")
-        )
-})
+            new ApiResponse(
+                200,
+                channelSubscribers,
+                "channel subscribers fetched successfully"
+            )
+        );
+});
 
-export {
-    toggleSubscription,
-    getUserChannelSubscribers,
-    getSubscribedChannels
-}
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params;
+
+    if (!subscriberId) {
+        throw new ApiError(400, "subscriberId is required");
+    }
+
+    if (!mongoose.Types.isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "not a valid objectId");
+    }
+
+    const channelsSubscribedByUser = await Subscription.find({
+        subscriber: subscriberId,
+    });
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                channelsSubscribedByUser,
+                "User subscribed channels fetched successfully"
+            )
+        );
+});
+
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
