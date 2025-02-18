6fe5182776999fb60f11667201b7864180117a82
codingashishdev <atankmember@gmail.com>
2025-02-18 11:50:46 +0530

    Revert "Implement healthcheck endpoint to return OK status with a message"
    This reverts commit 625abb6feb389889301fd9fdbc9e45c13bfadbe2.

--- Backend/src/controllers/healthcheck.controller.js
+++ Backend/src/controllers/healthcheck.controller.js
@@ -1,11 +1,12 @@
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
 
 const healthcheck = asyncHandler(async (req, res) => {
-    return res
-        .status(200)
-        .json(new ApiResponse(200, "OK", "Healthcheck is OK"));
-});
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+})
 
-export { healthcheck };
+export {
+    healthcheck
+}
